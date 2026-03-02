99d9bfa92bfafc376caf50f302807dd108add403
gajendra-ingle <gajendraingle01@gmail.com>
2026-03-02 18:44:41 +0530

    Refactor: update entity fields and improve mapping configuration

--- src/main/java/com/techpulse/learn/entity/User.java
+++ src/main/java/com/techpulse/learn/entity/User.java
@@ -1,18 +1,15 @@
 package com.techpulse.learn.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class User {
 
     private Long id;
     private String name;
     private String email;
+    private Long isDeleted; // 0 = active, 1 = deleted
 
-
-    public User(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 }
