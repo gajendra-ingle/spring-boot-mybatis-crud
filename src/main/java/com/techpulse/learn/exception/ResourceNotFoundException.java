abe5a47d29a992a66777e9a4ccfb56bec4158f2f
gajendra-ingle <gajendraingle01@gmail.com>
2026-03-02 18:43:05 +0530

    feat : create ResourceNotFoundException for missing resources

--- src/main/java/com/techpulse/learn/exception/ResourceNotFoundException.java
+++ src/main/java/com/techpulse/learn/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.techpulse.learn.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
