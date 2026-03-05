55e946faa778b5205504d427c36512642bef4015
gajendra-ingle <gajendraingle01@gmail.com>
2026-03-05 15:07:05 +0530

    refactor: update User entity to support sorting functionality

--- src/main/java/com/techpulse/learn/entity/User.java
+++ src/main/java/com/techpulse/learn/entity/User.java
@@ -3,6 +3,8 @@ package com.techpulse.learn.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class User {
@@ -11,5 +13,6 @@ public class User {
     private String name;
     private String email;
     private Long isDeleted; // 0 = active, 1 = deleted
+    private List<Order> orders;
 
 }
